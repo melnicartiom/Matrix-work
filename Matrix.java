d26e84e0d2a335b3c22317f0d1cbbadef1387c0d
Melnic Artiom <melnicartiom@gmail.com>
2018-12-15 11:55:41 +0200

    Basic changes

---
+++ Matrix.java
@@ -0,0 +1,133 @@
+
+public class Matrix {
+
+
+    public static int[][] fillTheMatrix (int[][] mtx){
+
+        for (int i = 0; i < mtx.length; i++) {
+
+            for (int j = 0; j < mtx[i].length; j++) {
+                mtx[i][j] = 0;
+            }
+
+        }
+        return mtx;
+    }
+
+    public static boolean randomFillTheMatrix (int[][] mtx){
+        for (int i = 0; i < mtx.length; i++) {
+            for (int j = 0; j < mtx[0].length; j++) {
+                mtx[i][j] = (int)(Math.random() * 10);
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean printTheMatrix (int[][] mtx){
+        for (int i = 0; i < mtx.length; i++) {
+            for (int j = 0; j < mtx[i].length; j++) {
+                System.out.print(mtx[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        return true;
+    }
+
+    public static boolean swapZeroToOneInPerimetr (int [][] mtx){
+        for (int i = 0; i <mtx.length ; i += mtx.length - 1) {
+            for (int j = 0; j <mtx[i].length ; j++) {
+                mtx[i][j] = 1;
+            }
+        }
+        for (int i = 0; i < mtx.length; i++) {
+            for (int j = 0; j < 1; j++) {
+                mtx[i][j] = 1;
+            }
+        }
+        for (int i = 0; i < mtx.length; i++) {
+            for (int j = mtx[i].length - 1; j > mtx[i].length - 2 ; j--) {
+                mtx[i][j] = 1;
+            }
+        }
+        return true;
+    }
+
+    public static boolean userMatrix (int[][] mtx){
+        int countNumbers = 0;
+
+        for (int i = 0; i < mtx.length; i++) {
+            for (int j = 0; j <mtx[i].length ; j++) {
+                mtx[i][j] = 1;
+                countNumbers++;
+            }
+        }
+        for (int i = 0; i < mtx.length; i++) {
+            for (int j = 0; j <mtx[i].length ; j++) {
+                System.out.print(mtx[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Количество 1 в массиве равно: " + countNumbers);
+
+        return true;
+    }
+
+
+    public static int[][] multiplyMatrix (int[][] m1, int[][] m2){
+        int rowLengthMat1 = m1.length;
+        int columnLengthMat2 = m2[0].length;
+
+        if (rowLengthMat1 != columnLengthMat2){
+            System.out.println("Imposible to multiply");
+            return null;
+        }
+
+        // строк как у первой - столбцов как у второй
+        int[][] mResult = new int[m1.length][m2[0].length];
+
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m2[i].length; j++) {
+                for (int k = 0; k < m1[i].length; k++) {
+                    mResult[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+        return mResult;
+    }
+
+    public static int[][] multiplyMatrixToInteger (int[][] mtx, int num){
+        int[][] resultMtx = new int[mtx.length][mtx[0].length];
+        for (int i = 0; i < mtx.length; i++) {
+            for (int j = 0; j < mtx[i].length ; j++) {
+                resultMtx[i][j] = mtx[i][j] * num;
+            }
+        }
+        return resultMtx;
+    }
+
+    public static int[][] sumMatrix (int[][] m1, int[][] m2){
+        int [][] resultM = new int[m1.length][m1[0].length];
+
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[i].length; j++) {
+                resultM[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+        return resultM;
+    }
+
+//    public static int[][] exponentialMatrix (int[][] mtx, int num){
+//        int[][] resultMtx = new int[mtx.length][mtx[0].length];
+//        for (int i = 0; i < mtx.length; i++) {
+//            for (int j = 0; j < mtx[0].length; j++) {
+//                for (int k = num; k > 0; k--) {
+//                    resultMtx[i][j] += mtx[i][j] * mtx[i][j];
+//                }
+//            }
+//        }
+//        return resultMtx;
+//    }
+
+
+}
---
+++ main.java
@@ -0,0 +1,61 @@
+import org.omg.CORBA.MARSHAL;
+
+public class main {
+    public static void main(String[] args) {
+
+//        int[][] mass = new int[4][4];
+//        Matrix.fillTheMatrix(mass);
+//        Matrix.printTheMatrix(mass);
+//
+//        Matrix.swapZeroToOneInPerimetr(mass);
+//        System.out.println("\n" + "New matrix :");
+//        Matrix.printTheMatrix(mass);
+
+//        int[][] mass = new int[5][5];
+//        Matrix.userMatrix(mass);
+
+//        int[][] mass1 = new int[2][3];
+//        Matrix.randomFillTheMatrix(mass1);
+//        System.out.println("Первая матрица: ");
+//        Matrix.printTheMatrix(mass1);
+//
+//        int[][] mass2 = new int[3][2];
+//        Matrix.randomFillTheMatrix(mass2);
+//        System.out.println("Вторая матрица: ");
+//        Matrix.printTheMatrix(mass2);
+//
+//        System.out.println("Результат умножения двух матриц: ");
+//        Matrix.printTheMatrix(Matrix.multiplyMatrix(mass1,mass2));
+
+
+        int[][] mass1 = new int[][] {
+                {3,5,7},
+                {8,9,1},
+                {3,4,5}
+        };
+        System.out.println("Первая матрица: ");
+        Matrix.printTheMatrix(mass1);
+
+//        int[][] mass2 = new int[][] {
+//                {2,4,9},
+//                {6,8,1},
+//                {3,5,7}
+//        };
+//        System.out.println("Вторая матрица: ");
+//        Matrix.printTheMatrix(mass2);
+
+//        System.out.println("Exponential matrix :");
+//        Matrix.printTheMatrix(Matrix.exponentialMatrix(mass1, 1));
+
+//        System.out.println("Sum two matrix: ");
+//        Matrix.printTheMatrix(Matrix.sumMatrix(mass1, mass2));
+
+//        System.out.println("Умножение на число первой матрицы: ");
+//        Matrix.printTheMatrix(Matrix.multiplyMatrixToInteger(mass1, 2));
+//
+//        System.out.println("Результат умножения двух матриц: ");
+//        Matrix.printTheMatrix(Matrix.multiplyMatrix(mass1,mass2));
+
+
+       }
+}
